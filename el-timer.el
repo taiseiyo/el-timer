aef98194615ab9470d9f3ce33c7a7bf79d24a34d
suzuki taisei <taisei@localhost>
2021-12-17 16:42:16 +0900

    fix

--- el-timer.el
+++ el-timer.el
@@ -1,13 +1,13 @@
-(progn
-  ;; (defun el-timer(end-time)
-  ;; (interactive "n:Enter end time: ")
+(defun el-timer-start(end-time)
+  (interactive "BEnter end time: ")
     (setq time-count 0
-	  tmp-line mode-line-format)
-
+	  tmp-line mode-line-format
+	  et end-time)
+    
     (defun mainloop()
       (setq time-count (+ time-count 1))
       (setq time (s-trim-right (shell-command-to-string
-			  (concat "el-timer -st " (number-to-string time-count)  " -et " "0.1"))))
+			  (concat "el-timer -st " (number-to-string time-count)  " -et " (number-to-string et)))))
       (setq time (concat time " "))
       (setq mode-line-format tmp-line)
       (setq under-line mode-line-format)
@@ -21,14 +21,15 @@
       )
     
     (setq el-timer (run-at-time 0 1 'mainloop))
-    ;; )
+    
     )
 
-(progn
-  ;; (defun cancel-el-timer()
-  ;; (interactive)
+(defun el-timer-stop()
+  (interactive)
     (cancel-timer el-timer)
     (setq mode-line-format tmp-line)
-    ;; )
   )
 
+(provide 'el-timer)
+;; 00:00 の 10 秒後に mini-buffer から time を消したい
+;; 出力に色付けたい -> defface ?
