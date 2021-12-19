cc01262a155a8c210f10436562d0107d28256ddd
suzuki taisei <taisei@localhost>
2021-12-19 21:01:57 +0900

    fix

--- el-timer.el
+++ el-timer.el
@@ -9,10 +9,10 @@
       (setq time (s-trim-right (shell-command-to-string
 				(concat "el-timer -st " (number-to-string time-count)  " -et " (number-to-string et)))))
       
-      (setq time (concat time " "))
-      (setq mode-line-format tmp-line)
-      (setq under-line mode-line-format)
-      (setq mode-line-format (cons time under-line))
+      (setq time (concat time " ")
+	    mode-line-format tmp-line
+	    under-line mode-line-format
+	    mode-line-format (cons time under-line))
 
       (if (equal (s-trim time) "00:00")
 	  (progn
@@ -27,10 +27,25 @@
 
 (defun el-timer-stop()
   (interactive)
-    (cancel-timer timer)
-    (setq mode-line-format tmp-line)
+  (cancel-timer timer)
+  
+  (setq start-window (selected-window)
+	end-window (next-window (selected-window))
+	window-counter 1)
+  
+  (while (equal nil (equal start-window end-window))
+    (other-window window-counter)
+    (setq mode-line-format tmp-line
+	  end-window (next-window end-window)
+    	  window-counter (+ window-counter 1)
+	  )
+    )
+  
+  (other-window (+ window-counter 1))
+  (setq mode-line-format tmp-line)
   )
 
 (provide 'el-timer)
+
 ;; 00:00 の 10 秒後に mini-buffer から time を消したい
 ;; 出力に色付けたい -> defface ?
