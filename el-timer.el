91e68b287e4e8a4890904d82fbc34cc8cfa1651c
suzuki taisei <taisei@localhost>
2022-02-18 19:11:51 +0900

    fix

--- el-timer.el
+++ el-timer.el
@@ -1,14 +1,8 @@
-(defun el-timer-start(end-time)
-  (interactive "NEnter end time: ")
-    (setq time-count 0
-	  tmp-line mode-line-format
-	  et end-time)
-
-    (defun mainloop()
+(defun mainloop()
       (setq time-count (+ time-count 1))
       (setq time (string-trim-right (shell-command-to-string
-				(concat "python3 " (getenv "HOME") "/bin/el-timer -st " (number-to-string time-count)  " -et " (number-to-string et)))))
-      
+				     (concat "python3 " (getenv "HOME") "/bin/el-timer -st " (number-to-string time-count)  " -et " (number-to-string et)))))
+
       (setq time (concat time " ")
 	    mode-line-format tmp-line
 	    under-line mode-line-format
@@ -21,15 +15,9 @@
 	    )
 	)
       )
-    
-    (setq timer (run-at-time 0 1 'mainloop))
-    
-    )
 
-(defun el-timer-stop()
-  (interactive)
-  (cancel-timer timer)
-  
+
+(defun delete-timer-mode-line()
   (setq start-window (selected-window)
 	end-window (next-window (selected-window))
 	mode-line-format tmp-line
@@ -43,6 +31,37 @@
     )
   
   (other-window 1)
+  )
+
+(defun el-timer-start(end-time)
+  "start timer. The time-count variable is used to control the timer."
+  (interactive "NEnter end time: ")
+    (setq time-count 0
+	  tmp-line mode-line-format
+	  et end-time
+	  timer (run-at-time 0 1 'mainloop))
+    )
+
+
+(defun el-timer-stop()
+  "stop timer"
+  (interactive)
+  (cancel-timer timer)
+  (delete-timer-mode-line)
+  )
+
+(defun el-timer-pause()
+  "pause timer"
+  (interactive)
+  (cancel-timer timer)
+  )
+
+(defun el-timer-restart()
+  "restart timer"
+  (interactive)
+  (delete-timer-mode-line)
+  (setq tmp-line mode-line-format
+	timer (run-at-time 0 1 'mainloop))
   )
 
 (provide 'el-timer)
